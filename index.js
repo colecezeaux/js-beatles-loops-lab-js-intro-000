2e3dc17ecc9ef0a63017a9d9aee3fa0f5365e31a
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 21:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(array) {
   var i = []
   for (let n = 0; n < musicians.length; n++) {
-    i.push(`${musicians} plays ${instruments}.`);
+    i.push(`${musicians[n]} plays ${instruments[n]}.`);
     }
   return i
 }
